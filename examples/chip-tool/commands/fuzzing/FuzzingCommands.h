3d321368470a4ada6b14719aecf20d39ad6a266c
w8floosh <martingibilterra@gmail.com>
2024-08-31 23:22:15 +0200

    feat: Integrated fuzzing command correctly in chip-tool
    Additions:
    - added DeviceConfiguration class and variadic type traits to create multilevel unordered maps
    - added custom application errors
    - adapted gn build files
    
    Modifications:
    - refactoring fuzzer initialization
    - little modifications to Oracle code
    - fixed segfaults in commands initialization

--- examples/chip-tool/commands/fuzzing/FuzzingCommands.h
+++ examples/chip-tool/commands/fuzzing/FuzzingCommands.h
@@ -1,7 +1,9 @@
+#pragma once
+
 #include "../clusters/DataModelLogger.h"
 #include "../common/CHIPCommand.h"
 #include "../common/Commands.h"
-#include "Fuzzing.h"
+#include "Fuzzers.h"
 
 namespace fuzz = chip::fuzzing;
 
@@ -16,10 +18,10 @@ public:
     /////////// CHIPCommand Interface /////////
     chip::System::Clock::Timeout GetWaitDuration() const override { return chip::System::Clock::Seconds16(0); }
     void ExecuteCommand(const char * command, int * status);
+    virtual CHIP_ERROR RunCommand() = 0;
 
 private:
     Commands * mHandler = nullptr;
-    chip::Optional<bool> mAdvertiseOperational;
 };
 
 class FuzzingStartCommand : public FuzzingCommand
@@ -30,89 +32,31 @@ public:
     {
 
         // Initializing fuzzing options, taking the following arguments from command line
-        char * aFuzzerType      = nullptr;
-        char * aFuzzingStrategy = nullptr;
-        char * aSeedDirectory   = nullptr;
-        char * aOutputDirectory = nullptr;
-        char * aIterations      = nullptr;
-
-        AddArgument("fuzzer", &aFuzzerType, "Fuzzer type (afl++, ...)");
-        AddArgument("fuzzing-strategy", &aFuzzingStrategy, "Fuzzing strategy");
-        AddArgument("seed-path", &aSeedDirectory, "Path where to read fuzzer seeds from and where to save correct commands");
-        AddArgument("log-path", &aOutputDirectory, "Path where to export stateful fuzzer logs. Enables stateful fuzzing");
-        AddArgument("iterations", &aIterations, "Number of iterations (commands) to run the fuzzer for");
-
-        VerifyOrDieWithMsg((aFuzzerType == nullptr) == (aFuzzingStrategy == nullptr), chipFuzzer,
-                           "Both fuzzer type and strategy must be specified to enable fuzzing");
-
-        fuzz::FuzzerType * kFuzzerType = fuzz::ConvertStringToFuzzerType(aFuzzerType);
-        VerifyOrDieWithMsg((aFuzzerType != nullptr) != (kFuzzerType == nullptr), chipFuzzer,
-                           "Specified fuzzer type is not implemented");
-
-        fuzz::FuzzingStrategy * kFuzzingStrategy = fuzz::ConvertStringToFuzzingStrategy(aFuzzingStrategy);
-        VerifyOrDieWithMsg((aFuzzingStrategy != nullptr) != (kFuzzingStrategy == nullptr), chipFuzzer,
-                           "Specified fuzzer type is not implemented");
-
-        VerifyOrDieWithMsg(aSeedDirectory != nullptr, chipFuzzer, "Seed path must be specified");
-
-        mSeedDirectory = fs::path(aSeedDirectory);
-        free(aSeedDirectory);
-        aSeedDirectory = nullptr;
-        VerifyOrDieWithMsg(fs::exists(mSeedDirectory), chipFuzzer, "Seed path does not exist");
-
-        if (aIterations != nullptr)
-        {
-            mIterations = atoi(aIterations);
-            free(aIterations);
-            aIterations = nullptr;
-        }
-
-        if (aOutputDirectory != nullptr)
-        {
-            mStatefulFuzzingEnabled = true;
-            mOutputDirectory.SetValue(fs::path(aOutputDirectory));
-            free(aOutputDirectory);
-            aOutputDirectory = nullptr;
-            VerifyOrDieWithMsg(fs::exists(mOutputDirectory.Value()), chipFuzzer, "Output path does not exist");
-        }
-
-        // Fuzzer instance initialization
-        CHIP_ERROR err;
-
-        if (mStatefulFuzzingEnabled)
-        {
-            err = fuzz::Init(*kFuzzerType, *kFuzzingStrategy, mSeedDirectory, mOutputDirectory.Value(), &mFuzzer);
-        }
-        else
-        {
-            err = fuzz::Init(*kFuzzerType, *kFuzzingStrategy, mSeedDirectory, &mFuzzer);
-        }
-
-        if (kFuzzerType != nullptr)
-        {
-            free(kFuzzerType);
-            kFuzzerType = nullptr;
-        }
-
-        if (kFuzzingStrategy != nullptr)
-        {
-            free(kFuzzingStrategy);
-            kFuzzingStrategy = nullptr;
-        }
-
-        VerifyOrDieWithMsg(err == CHIP_NO_ERROR, chipFuzzer, "Failed to initialize fuzzer");
+        AddArgument("fuzzer", &mFuzzerTypeArgument, "Fuzzer type (afl++, ...)");
+        AddArgument("strategy", &mFuzzingStrategyArgument, "Fuzzing strategy");
+        AddArgument("seed-path", &mSeedDirectoryArgument,
+                    "Path where to read fuzzer seeds from and where to save correct commands");
+        AddArgument("output-path", &mOutputDirectoryArgument,
+                    "Path where to export stateful fuzzer logs. Enables stateful fuzzing");
+        AddArgument("iterations", 0U, UINT32_MAX, &mIterations, "Number of iterations (commands) to run the fuzzer for");
     }
 
     /////////// CHIPCommand Interface /////////
     CHIP_ERROR RunCommand() override;
 
 private:
+    char * mFuzzerTypeArgument;
+    char * mFuzzingStrategyArgument;
+    char * mSeedDirectoryArgument;
+    chip::Optional<char *> mOutputDirectoryArgument = chip::NullOptional;
+    chip::Optional<uint32_t> mIterations            = chip::Optional<uint32_t>::Value(1000);
+
     fuzz::Fuzzer * mFuzzer;
-    int mIterations              = 1000;
     bool mStatefulFuzzingEnabled = false;
     fs::path mSeedDirectory;
     chip::Optional<fs::path> mOutputDirectory = chip::NullOptional;
 
+    CHIP_ERROR InitializeFuzzer();
     CHIP_ERROR RetrieveNodeDescription(chip::NodeId node);
-    char * GenerateCommand(chip::ClusterId cluster);
+    const char * GenerateCommand(chip::ClusterId cluster);
 };
